5791911ab58bc3fd948711e0ef76e4e5e019258e
carsonlevine <carsonglevine@me.com>
2016-07-08 15:17:14 -0400

    first commit

---
+++ ArrayMethodsPractice.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var message = "Let's practice building some arrays! Make an array holding the first names of the students at your table."
+
+//Create the array here. Name the array 'students1'.
+
+
+
+
+
+//Create another array to hold the names of the students at the table next to you. Call this 'students2'.
+
+
+
+
+
+//Make up a name and insert it into the second position of students1
+
+
+
+
+
+//Make up another name and add it to the end of students2
+
+
+
+
+
+//Delete the last element of students1
+
+
+
+
+
+//Remove the third name in students2
+
+
+
+
+
+//Now combine the groups. Put all of the names in the students1 array.
+
+
+
+
+
+//Make a variable called numberOfStudents and store the total number of students in it.
